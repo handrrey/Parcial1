cdc0ad4659dfe41c866d689c04de550d69b0616d
handrrey <andrea.henao@utp.edu.co>
2025-03-06 23:43:21 -0500

    Add files via upload

--- limpiador/limpiar.py
+++ limpiador/limpiar.py
@@ -1,14 +1,22 @@
 import pandas as pd
-import numpy as np
+
 
 def limpiar(df_limpio):
     columnasInteres = ["pH agua:suelo 2,5:1,0", "Fósforo (P) Bray II mg/kg",
-                            "Potasio (K) intercambiable cmol(+)/kg"]
+                       "Potasio (K) intercambiable cmol(+)/kg"]
 
     for col in columnasInteres:
+        if col in df_limpio.columns:
+            # Limpieza de datos
+            df_limpio[col] = df_limpio[col].astype(str)
+            df_limpio[col] = df_limpio[col].str.replace(',', '', regex=False)
+            df_limpio[col] = df_limpio[col].str.replace('<', '', regex=False)
+            df_limpio[col] = df_limpio[col].str.replace('>', '', regex=False)
 
-                df_limpio[col] = df_limpio[col].astype(str)
-                df_limpio[col] = df_limpio[col].str.replace(',', '')
-                df_limpio[col] = df_limpio[col].str.replace('<', '').str.replace('>', '')
-    
+
+            df_limpio[col] = pd.to_numeric(df_limpio[col], errors='coerce')
+        else:
+            print(f"Advertencia: La columna {col} no se encuentra en el DataFrame.")
+
     return df_limpio
+
